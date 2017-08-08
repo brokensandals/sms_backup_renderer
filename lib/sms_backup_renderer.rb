86761fc2921fbe4639926f50d1ef33913ce08335
Jacob Williams <jacobaw@gmail.com>
2017-08-07 21:27:44 -0500

    Implement a basic dump to HTML

--- lib/sms_backup_renderer.rb
+++ lib/sms_backup_renderer.rb
@@ -1,7 +1,9 @@
 require 'base64'
 require 'sms_backup_renderer/version'
 require 'date'
+require 'erb'
 require 'nokogiri'
+require 'pathname'
 require 'securerandom'
 
 module SmsBackupRenderer
@@ -175,6 +177,27 @@ module SmsBackupRenderer
       true
     else
       raise "Unrecognized MMS m_type #{type}"
+    end
+  end
+
+  class IndexPage
+    # Returns the String directory to which all file paths in the generated HTML should be relative.
+    attr_reader :base_path
+
+    # Returns an Array of Arrays of Message instances.
+    attr_reader :message_groups
+
+    def initialize(base_path, message_groups)
+      @base_path = base_path
+      @message_groups = message_groups
+    end
+
+    def render
+      ERB.new(File.read(File.join(File.dirname(__FILE__), 'sms_backup_renderer', 'templates', 'index.html.erb'))).result(binding)
+    end
+
+    def relative_path(path)
+      Pathname.new(path).relative_path_from(Pathname.new(base_path)).to_s
     end
   end
 end

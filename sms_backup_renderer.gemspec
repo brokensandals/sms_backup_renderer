36119f5419dd5ec81e1fc461121f38b596fd7e19
Jacob Williams <jacobaw@gmail.com>
2017-08-23 18:46:33 -0500

    Add very basic test

--- sms_backup_renderer.gemspec
+++ sms_backup_renderer.gemspec
@@ -32,4 +32,5 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'nokogiri', '~> 1.8'
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency 'rspec', '~> 3.6'
 end
---
+++ spec/sms_backup_renderer_spec.rb
@@ -0,0 +1,63 @@
+require 'sms_backup_renderer'
+
+# Just a basic integration test.
+RSpec.describe SmsBackupRenderer do
+  describe '.generate_html_from_archive' do
+    output_dir_path = File.join(__dir__, '..', 'tmp', 'test-output')
+    assets_dir_path = File.join(output_dir_path, 'assets')
+    conversations_dir_path = File.join(output_dir_path, 'conversations')
+    data_dir_path = File.join(output_dir_path, 'data')
+    index_page_path = File.join(output_dir_path, 'index.html')
+    conversation_page_path = File.join(conversations_dir_path, '2345678910.html')
+
+    before :all do
+      FileUtils.rm_r(output_dir_path) if File.exist?(output_dir_path)
+      SmsBackupRenderer.generate_html_from_archive(File.join(__dir__, 'test_archive.xml'), output_dir_path)
+    end
+
+    [output_dir_path,
+     assets_dir_path,
+     conversations_dir_path,
+     data_dir_path,
+     index_page_path,
+     conversation_page_path].each do |path|
+      it "creates #{path}" do
+        expect(File.exist?(path)).to be true
+      end
+    end
+
+    describe 'index.html' do
+      contents = nil
+
+      before :all do
+        contents = File.read(index_page_path)
+      end
+
+      it 'contains link to conversation page' do
+        expect(contents).to include 'conversations/2345678910.html'
+      end
+    end
+
+    describe '2345678910.html' do
+      contents = nil
+
+      before :all do
+        contents = File.read(conversation_page_path)
+      end
+
+      it 'contains first message date' do
+        # FIXME should mock the timezone before generating the file, then assert a literal value here
+        expect(contents).to include Time.parse('2017-04-01T16:05:13.000Z').getlocal
+          .strftime('%A, %b %-d, %Y at %-I:%M%P %Z')
+      end
+
+      it 'contains first message text' do
+        expect(contents).to include 'unfamiliar with the concept of cats'
+      end
+    end
+
+    after :all do
+      FileUtils.rm_r(output_dir_path) unless ENV['KEEP_TEST_OUTPUT']
+    end
+  end
+end
